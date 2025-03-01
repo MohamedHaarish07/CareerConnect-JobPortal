778022419c9538ea252d81b164620b9feff6b5da
MohamedHaarish07 <mohamedhaarish55@gmail.com>
2025-03-01 19:17:47 +0530

    firstest commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
+import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js'
+
 
 //initialize express
 const app=express()
+//connect to db
+await connectDB()
 
 //Middlewares
 app.use(cors())
@@ -11,9 +18,15 @@ app.use(express.json())
 
 //Routes
 app.get('/',(req, res)=>res.send("API Working"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+app.post('/webhoos',clerkWebhooks)
+
 
 //Port
 const PORT = process.env.PORT || 5000
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
